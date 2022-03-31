f3d1fc28a954ab4d50128636201fe77be7fc4585
Nick Kushkbaghi <nick.kushkbaghi@gmail.com>
2022-03-31 17:35:06 +0200

    Final

--- src/container/Contact/Contact.jsx
+++ src/container/Contact/Contact.jsx
@@ -5,10 +5,12 @@ import { Form } from "../../component";
 
 export default function Contact() {
   return (
-    <section className="contact">
-      <img className="sec-img" src={mailbox} alt="en post loda med brev" />
+    <section id="start" className="contact">
+      <img className="sec-img" src={mailbox} alt="en postlåda med brev" />
       <h2>Kontakta mig</h2>
+      
       <Form/>
+      
     </section>
   );
 }
