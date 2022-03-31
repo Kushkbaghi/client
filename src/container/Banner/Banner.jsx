b72a01ced5dffafccba6b292fcaad31fede7ea62
Nick Kushkbaghi <nick.kushkbaghi@gmail.com>
2022-03-31 20:36:36 +0200

    final

--- src/container/Banner/Banner.jsx
+++ src/container/Banner/Banner.jsx
@@ -6,39 +6,42 @@ import {
   AiFillGithub,
 } from "react-icons/ai";
 import { banner } from "../../images";
+import {motion} from "framer-motion";
 export default function Banner() {
   return (
     <section id="start" className="banner">
-      <div className="banner__img">
-        <img
-          src={banner}
-          alt="karton av två man och en kvinna och CSS, HTML och CSS logo"
-        />
-      </div>
-      <div className="banner__CTA">
-        <div className="banner__CTA-titles">
-          <h1>Hej! Jag heter Nick.</h1>
-          <p>Jag är en fullstack-utvecklare, välllkommen till min sida!</p>
+     
+        <div className="banner__img">
+          <img
+            src={banner}
+            alt="karton av två man och en kvinna och CSS, HTML och CSS logo"
+          />
         </div>
-        <div className="banner__CTA-links">
-          <a href="#contact" className="banner__CTA--kontakt">
-            Kontakt
-          </a>
+        <div className="banner__CTA">
+          <div className="banner__CTA-titles">
+            <h1>Hej! Jag heter Nick.</h1>
+            <p>Jag är en fullstack-utvecklare, välllkommen till min sida!</p>
+          </div>
+          <div className="banner__CTA-links">
+            <a href="#contact" className="banner__CTA--kontakt">
+              Kontakt
+            </a>
 
-          <div className="banner__CTA-media">
-            <a href="#">
-              <AiFillTwitterCircle />
-            </a>
-            <a href="#">
-              <AiFillLinkedin />
-            </a>
-            <a href="#">
-              {" "}
-              <AiFillGithub />
-            </a>
+            <div className="banner__CTA-media">
+              <a href="#">
+                <AiFillTwitterCircle />
+              </a>
+              <a href="#">
+                <AiFillLinkedin />
+              </a>
+              <a href="#">
+                {" "}
+                <AiFillGithub />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      
     </section>
   );
 }
